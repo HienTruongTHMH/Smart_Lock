54dea7bc1b190d10545d8aca8523b093d77670ff
HienTruong <hien0934752432@gmail.com>
2025-06-20 00:04:05 +0700

    fix: update CORS origin URL for consistency with deployment

--- api-iot/api/_cors.js
+++ api-iot/api/_cors.js
@@ -1,5 +1,5 @@
 export function setupCors(res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://api-iot-v2-61qtwacr7-hiens-projects-d1689d2e.vercel.app'); // Hoặc 'https://smart-lock-by-git.vercel.app'
+  res.setHeader('Access-Control-Allow-Origin', 'https://smart-lock-by-git.vercel.app/'); // Hoặc 'https://smart-lock-by-git.vercel.app'
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
